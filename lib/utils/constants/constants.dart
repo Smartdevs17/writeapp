8e3bbb456690bb117978df6f7ae727a100033f28
smartdev <smartdevs17@gmail.com>
2024-07-02 09:51:29 +0100

    added the server side base url

--- lib/utils/constants/constants.dart
+++ lib/utils/constants/constants.dart
@@ -6,12 +6,14 @@ class Constants {
   //Starlink url
   // static const String baseUrl = "http://192.168.1.187:3000/api";
   //Myhot Spot url
-  static const String baseUrl = "http://192.168.225.80:3000/api";
+  // static const String baseUrl = "http://192.168.225.80:3000/api";
   //MyAirtel url
   // static const String baseUrl = "http://192.168.1.179:3000/api";
   //MyMtn Mifi
   // static const String baseUrl = "http://192.168.0.187:3000/api";
 
+  //Server Error
+  static const String baseUrl = "https://writeapp-api.onrender.com/api";
   // AUTH
   static const String STGW = 'Something gone wrong';
   static const String USER_EXIST = 'User already exist';
